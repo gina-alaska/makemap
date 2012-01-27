eb4c064e2c46f6d9d26efb216123f08944211ba1
Jason Grimes <jgrimes@hermes.gina.alaska.edu>
2012-01-26 16:28:08 -0900

    Adding json responce list for saved maps

--- app/controllers/makemaps_controller.rb
+++ app/controllers/makemaps_controller.rb
@@ -1,19 +1,27 @@
 class MakemapsController < ApplicationController
 
   def show
-    map = MakeMapWMS.new params
-    redirect_to map.to_s, :status => 302
+    map = MapSave.where( :id => params["id"] )
+    logger.info map.inspect
+    respond_to do |format|
+      format.json { render :json => map }
+    end
   end
 
   def create
     mapsave = MapSave.new params["image"]
     map = MakeMapWMS.new params["image"]
     logger.info params.inspect
-#    respond_to do |format|
-#      format.json { render :json => {"success"=>true} }
-#    end
     if mapsave.save!
       redirect_to map.to_s, :status => 302
+    end
+  end
+
+  def index
+    maps = MapSave.all
+    logger.info maps.inspect
+    respond_to do |format|
+      format.json { render :json => maps }
     end
   end
 
