146d3311accbaf45aa957832508faa88dfa4af1f
Will Fisher <will@teknofire.net>
2012-09-11 15:43:51 -0800

    Add some help text for initializer

--- config/initializers/map.rb
+++ config/initializers/map.rb
@@ -1,1 +1,3 @@
+# Set this to change the default map configuration
+# Valid options are: Alaska - EPSG:3338 (default), Alaska - EPSG:3572, USA - EPSG:3857
 Makemap::Application.config.map_site = 'Alaska - EPSG:3338'
