c90b40e077630531efa6e5e350d1e25a75f44a6c
Will Fisher <will@teknofire.net>
2012-09-11 15:45:13 -0800

    Add map initializer to ignore

--- config/initializers/map.rb
+++
@@ -1,3 +0,0 @@
-# Set this to change the default map configuration
-# Valid options are: Alaska - EPSG:3338 (default), Alaska - EPSG:3572, USA - EPSG:3857
-Makemap::Application.config.map_site = 'Alaska - EPSG:3338'
